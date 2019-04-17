38163176c9fa1fe254ca1bf6f351300ce5bca45b
vamsis-adapa <vamsis.adapa@gmail.com>
2019-04-17 22:55:33 +0530

    Revert "#fixes commands in gym.sql file"
    This reverts commit 77639effbda0e955b5d84b59bca4d2ac925ffa61.

--- gym.sql
+++ gym.sql
@@ -1,8 +1,6 @@
-CREATE DATABASE gym;
-
-USE gym;
-
 CREATE TABLE info(username VARCHAR(200), password VARCHAR(500), name VARCHAR(100), prof INT, street VARCHAR(100), city VARCHAR(50), phone VARCHAR(32), PRIMARY KEY(username));
+
+--CREATE TABLE members(username VARCHAR(200), plan VARCHAR(100), trainor VARCHAR(200), FOREIGN KEY(username) references info(username), PRIMARY KEY(username), FOREIGN KEY(trainor) references info(username));
 
 CREATE TABLE plans(name VARCHAR(100),exercise VARCHAR(20),sets int,reps int, PRIMARY KEY(name));
 
@@ -12,7 +10,9 @@ CREATE TABLE trainors(username VARCHAR(200), PRIMARY KEY(username), FOREIGN KEY(
 
 CREATE TABLE members(username VARCHAR(200), plan VARCHAR(100), trainor VARCHAR(200), PRIMARY KEY(username), FOREIGN KEY(username) references info(username), FOREIGN KEY(plan) references plans(name), FOREIGN KEY(trainor) references trainors(username));
 
-ALTER TABLE info ADD time TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE info ADD time TIMESTAMP DEFAULT CURRENT_TIMESTAMP;--done for all tables
+
+INSERT INTO info(username, password, name, prof, street, city, phone) VALUES('eswar_123', '$5$rounds=535000$ajR8hAzSoSF.NhEs$MaLn1dbnXq9eu2W5Ge3c1ScAS9960yLBFv3aU9zaxc0', 'Parameswar K', 1, 'Adarshnagar', 'Anantapur', 9666585361);--admin's password is eswar@259522
 
 CREATE TABLE progress(username VARCHAR(200), date DATE, daily_result VARCHAR(200), time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,rate int, PRIMARY KEY(username, date), FOREIGN KEY(username) references members(username));
 
@@ -21,9 +21,9 @@ INSERT INTO info(username, password, street, city, phone, name, prof)
  	'Adarshnagar', 'Anantapur', '9666585361', 'Parameswar Kurakula', 1);
  INSERT INTO plans(name, exercise, sets, reps)
  VALUES('500 per month','dribs',4,20);
- INSERT INTO plans(name, exercise, sets, reps)
+ INSERT INTO plans(name)
  VALUES('1000 per 3month','dribs',4,20);
- INSERT INTO plans(name, exercise, sets, reps)
+ INSERT INTO plans(name)
  VALUES('7000 per 5month','dribs',4,20);
  CREATE	 TABLE equip(name VARCHAR(20),count INT,PRIMARY KEY(name));
  INSERT INTO equip(name,count)
