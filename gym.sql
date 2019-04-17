a92d07a9dc69c9629f590eff3c0166bf186f147c
vamsis-adapa <vamsis.adapa@gmail.com>
2019-04-17 22:59:41 +0530

    Revert "Revert "#fixes commands in gym.sql file""
    This reverts commit 38163176c9fa1fe254ca1bf6f351300ce5bca45b.

--- gym.sql
+++ gym.sql
@@ -1,6 +1,8 @@
-CREATE TABLE info(username VARCHAR(200), password VARCHAR(500), name VARCHAR(100), prof INT, street VARCHAR(100), city VARCHAR(50), phone VARCHAR(32), PRIMARY KEY(username));
+CREATE DATABASE gym;
 
---CREATE TABLE members(username VARCHAR(200), plan VARCHAR(100), trainor VARCHAR(200), FOREIGN KEY(username) references info(username), PRIMARY KEY(username), FOREIGN KEY(trainor) references info(username));
+USE gym;
+
+CREATE TABLE info(username VARCHAR(200), password VARCHAR(500), name VARCHAR(100), prof INT, street VARCHAR(100), city VARCHAR(50), phone VARCHAR(32), PRIMARY KEY(username));
 
 CREATE TABLE plans(name VARCHAR(100),exercise VARCHAR(20),sets int,reps int, PRIMARY KEY(name));
 
@@ -10,9 +12,7 @@ CREATE TABLE trainors(username VARCHAR(200), PRIMARY KEY(username), FOREIGN KEY(
 
 CREATE TABLE members(username VARCHAR(200), plan VARCHAR(100), trainor VARCHAR(200), PRIMARY KEY(username), FOREIGN KEY(username) references info(username), FOREIGN KEY(plan) references plans(name), FOREIGN KEY(trainor) references trainors(username));
 
-ALTER TABLE info ADD time TIMESTAMP DEFAULT CURRENT_TIMESTAMP;--done for all tables
-
-INSERT INTO info(username, password, name, prof, street, city, phone) VALUES('eswar_123', '$5$rounds=535000$ajR8hAzSoSF.NhEs$MaLn1dbnXq9eu2W5Ge3c1ScAS9960yLBFv3aU9zaxc0', 'Parameswar K', 1, 'Adarshnagar', 'Anantapur', 9666585361);--admin's password is eswar@259522
+ALTER TABLE info ADD time TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
 
 CREATE TABLE progress(username VARCHAR(200), date DATE, daily_result VARCHAR(200), time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,rate int, PRIMARY KEY(username, date), FOREIGN KEY(username) references members(username));
 
@@ -21,9 +21,9 @@ INSERT INTO info(username, password, street, city, phone, name, prof)
  	'Adarshnagar', 'Anantapur', '9666585361', 'Parameswar Kurakula', 1);
  INSERT INTO plans(name, exercise, sets, reps)
  VALUES('500 per month','dribs',4,20);
- INSERT INTO plans(name)
+ INSERT INTO plans(name, exercise, sets, reps)
  VALUES('1000 per 3month','dribs',4,20);
- INSERT INTO plans(name)
+ INSERT INTO plans(name, exercise, sets, reps)
  VALUES('7000 per 5month','dribs',4,20);
  CREATE	 TABLE equip(name VARCHAR(20),count INT,PRIMARY KEY(name));
  INSERT INTO equip(name,count)
